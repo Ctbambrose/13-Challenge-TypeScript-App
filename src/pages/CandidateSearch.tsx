b84f49f51c973594695e5246950411e083169a7b
Ctbambrose <ctbambrose@gmail.com>
2025-05-24 12:33:28 -0500

    got functioning NAV

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,11 @@
-import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+//import { useState, useEffect } from 'react';
+//import { searchGithub, searchGithubUser } from '../api/API';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  return (
+  <h1>CandidateSearch</h1>
+
+  )
 };
 
 export default CandidateSearch;
